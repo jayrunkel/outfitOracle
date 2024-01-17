44888ac602fcfbb41db0c478f6bfcf2d0776ddf0
MongoPete <119877710+MongoPete@users.noreply.github.com>
2024-01-17 12:15:45 -0500

    Updated prompts

--- api/prompt.py
+++ api/prompt.py
@@ -32,6 +32,17 @@ def prompt(data):
   #search the customerID (let's hardcode this value maybe for POC?)
   # consider the document model for the customerID - and pull attributes that could be useful for the user prompt.
   # example: 
+
+
+  '''
+    user_prompt1 = "I'm a casually stylish male with a light tan skin tone, looking for an outfit for a 'spy agents' themed company year-end party. My sizes are medium shirts, 32 waist pants, and my budget is up to $200. I prefer minimalist watches and sleek sunglasses for accessories, and comfortable loafers for footwear. The party is indoor, semi-formal, in cool weather. I like incorporating classic black and grey colors for the theme, and my shoe size is 10."
+
+    user_prompt2 = "I'm a casually fashionable female with a fair skin tone, seeking an ensemble for a 'spy agents' themed corporate end-of-year celebration. My clothing sizes vary: small tops, size 28 pants, with a budget of up to $250. I have a penchant for elegant bracelets and chic eyewear as accessories, paired with snug ankle boots for shoes. The event is an indoor, semi-formal affair in a cooler climate. I'm fond of blending traditional black and grey hues to suit the theme, and my shoe size is 8."
+
+
+    user_prompt3 = "I'm a woman planning to attend an upcoming EDM concert and I'm looking for some fashion advice. I want an outfit that's stylish, comfortable for dancing, and suitable for the energetic and vibrant atmosphere of an EDM event. I'm open to bold colors and unique designs that stand out in a crowd. Additionally, I'd appreciate suggestions for practical yet fashionable footwear, as I'll be on my feet most of the time. Any tips on accessories that complement the outfit and enhance the overall concert experience would also be welcome. My budget is $2000 and I'm a size 8."
+  '''
+
   customerprofile ='''
   {
     "_id": {
@@ -61,44 +72,43 @@ def prompt(data):
   logging.info('Sending a test completion job')
 
   json_data = '''
-  [
-      {
-          "outfit_articles": ["dark blue jeans", "black shoes", "black shirt", "black tie", "black suit", "black sunglasses", "black leather gloves", "silver watch"],
-          "attribute_extract": {
-              "gender": "Women",
-              "masterCategory": "Accessories",
-              "subCategory": "Watches",
-              "articleType": "Watches",
-              "baseColour": "Black",
-              "season": "Winter",
-              "usage": "Casual",
-              "price": 63.77,
-              "averageRating": 4.73
+      //This is an example template for the JSON object that we need to pass through MongoDB that you should return and are not default values.
+      //Order the outfit_articles from head to toe. THIS IS IMPORTANT! Ensure customerprofile is included in the attribute_extract object.
+      // DO NOT INCLUE any COMMENT BLOCKS THAT BREAKS JSON!!!. ENSURE IT IS VALID JSON. 
+      // ... means that you can add more details
+    {
+      "customerprofile": { ... },
+      [
+          {
+              "outfit_articles": ["dark blue jeans", "black shoes", "black shirt", "black tie", "black suit", "black sunglasses", "black leather gloves", "silver watch"],
+              "attribute_extract": { 
+                  "gender": "" ,
+                  "masterCategory": "Accessories",
+                  "subCategory": "Watches",
+                  "articleType": "Watches",
+                  "baseColour": "<Explicitly use color names not code versions>",
+                  "season": "<based on user prompt, if not, make best guess>",
+                  "usage": "Casual",
+                  "price": 63.77,
+                  "averageRating": 4.73,
+                  ...
+              },
+              "gpt_response": " You're a fasion advisor and don't reference customerprofile explicitly . Provide Text description / justification of this outfit and how it fits the prompt in twitter size ( up to 280 characters) - Exclude hashtags. ",
+              "dalle_prompt": "<You are a DALL-E image generator that will> Create this image with <insert outfit articles here. Ensure the articles of clothing is illustrated from head to toe. Ensure that it's using the customerprofile information to fill in the gaps of the person. specify based on the attribute_extract and prompt but prioritize the customerprofile information. it's paramount to describe head to toe of the complete outfit>. Show "Display a full-body view" or "Present a complete outfit from head to toe""
           },
-          "gpt_response": "Text description / justification of this outfit and how it fits the prompt",
-          "dalle_prompt": "Modify this image with <insert outfit articles here>"
-      },
-      ...
-  ]
-
-  '''
+          ...
+      ]
+    }
+    '''
 
   # gpt_instructions = "Give me a JSON form of an array of clothing based on the following prompt: "
-  gpt_instructions = "I am passing this to an API, so your response must be in valid JSON format. Using the user prompt below, create 3 outfits and embed them into a master array. If the user has a customerprofile object:" + customerprofile + ", include the attributes from the customerprofile object in the attribute_extract object.Ensure that the json_data is valid JSON.Minify the JSON object before submitting it as a response. The final output must be formatted in VALID JSON as follows: \n'''" + \
-      json_data + "'''\n The comments are only there for your instruction and should be left out of the response. Any attributes that are applicable to all articles should be included in the attribute_extract object. Add some variance and be sure that no outfit should share more than 3 of the same articles."
-
-  gpt_instructions1 = "Based on the user prompt, create 3 array of outfits (valid JSON Objects) that follow the formatting:"+ json_data + " Any attributes that are applicable to all outfits should be included in the attribute_extract object. If the user has a customerprofile object:" + customerprofile + ", include the attributes from the customerprofile object in the attribute_extract object.Ensure that the json_data is valid JSON.Minify the JSON object before submitting it as a response."
-
-  user_prompt1 = "I'm a casually stylish male with a light tan skin tone, looking for an outfit for a 'spy agents' themed company year-end party. My sizes are medium shirts, 32 waist pants, and my budget is up to $200. I prefer minimalist watches and sleek sunglasses for accessories, and comfortable loafers for footwear. The party is indoor, semi-formal, in cool weather. I like incorporating classic black and grey colors for the theme, and my shoe size is 10."
-
-  user_prompt2 = "I'm a casually fashionable female with a fair skin tone, seeking an ensemble for a 'spy agents' themed corporate end-of-year celebration. My clothing sizes vary: small tops, size 28 pants, with a budget of up to $250. I have a penchant for elegant bracelets and chic eyewear as accessories, paired with snug ankle boots for shoes. The event is an indoor, semi-formal affair in a cooler climate. I'm fond of blending traditional black and grey hues to suit the theme, and my shoe size is 8."
+  gpt_instructions = "I " + customerprofile +" am passing this to an API, so your response must be in valid JSON format. Using the user prompt below, create 3 outfits and embed them into a master array. If the user has a customerprofile object:, overwrite the attributes from the customerprofile object in the attribute_extract object.Ensure that the json_data is valid JSON.Minify the JSON object before submitting it as a response. The final output must be formatted in VALID JSON (values are just examples) as follows: \n'''" + \
+      json_data + "'''\n The comments are only there for your instruction and should be left out of the response. Any attributes that are applicable to all articles should be included in the attribute_extract object but also prioritize any data from"+ customerprofile +". Add some variance and be sure that no outfit should share more than 3 of the same articles. "
 
 
-  user_prompt3 = "I'm a woman planning to attend an upcoming EDM concert and I'm looking for some fashion advice. I want an outfit that's stylish, comfortable for dancing, and suitable for the energetic and vibrant atmosphere of an EDM event. I'm open to bold colors and unique designs that stand out in a crowd. Additionally, I'd appreciate suggestions for practical yet fashionable footwear, as I'll be on my feet most of the time. Any tips on accessories that complement the outfit and enhance the overall concert experience would also be welcome. My budget is $2000 and I'm a size 8."
-
-  user_prompt4 = "I want an outfit that's stylish, comfortable for dancing, and suitable for the energetic and vibrant atmosphere of an EDM event. I'm open to bold colors and unique designs that stand out in a crowd. Additionally, I'd appreciate suggestions for practical yet fashionable footwear, as I'll be on my feet most of the time. Any tips on accessories that complement the outfit and enhance the overall concert experience would also be welcome. My budget is $2000 and I'm a size 8." + customerprofile
 
   complete_prompt = gpt_instructions + " User prompt:" + user_prompt
+
 
   try:
       # Send a completion call to generate an answer
@@ -110,8 +120,11 @@ def prompt(data):
 
       # Extract and print text response
       response_text = response['choices'][0]['text'].strip()
+      # Remove any lines from response text that start with '//'
+
       logging.info("Raw Response Text:")
       logging.info(response_text)
+
 
       # Parse the response as JSON
       json_data = json.loads(response_text)
@@ -130,3 +143,6 @@ def prompt(data):
       logging.warning("Error context:", e.doc[e.pos-20:e.pos+20])  # Adjust the range as needed
   except Exception as e:
       logging.warning("An error occurred:", str(e))
+
+
+    
---
+++ api/prompt_img.py
@@ -0,0 +1,25 @@
+import os
+import openai
+import pprint
+openai.api_key = "45eefc80e0914e64995ed91c3c7cf175"
+openai.api_base = "https://outfitoracle.openai.azure.com/" # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
+openai.api_type = 'azure'
+openai.api_version = "2023-09-15-preview" # this might change in the future
+
+
+# Generate image for the outfit based on "dalle_prompt"
+# userprompt = "Modify this image with black slim fit blazer, black turtleneck, dark grey dress pants, black leather derby shoes, black leather band watch, and sleek black sunglasses."
+
+# userprompt ="Create this image with a grey tweed blazer, a black turtleneck sweater, and dark blue jeans. Add grey loafers, a silver watch, sleek black sunglasses, and black leather gloves for a complete look. The individual should exhibit a blend of casual and festive fashion, fitting for a 'spy agents' themed company year-end party. Age: 58 Heritage: German Gender: Male Skin Tone: Light tan Favorite Color: Black and grey. Outfit: The clothing should include a top, pants, and shoes, all reflecting a balance between casual style and party attire. The entire outfit should be visible in the image, providing a clear view from head to toe. The overall look should capture the essence of a spy, tailored to the individual's preferences."
+
+dalle_prompt = "Create an image featuring a person of non-binary gender wearing an outfit consisting of a red graphic t-shirt, black skinny jeans, white leather high tops, a black leather backpack, a silver chain bracelet, a black beanie, black round sunglasses, and silver stud earrings. Show the full body from head to toe."
+
+response = openai.Image.create(
+    prompt= dalle_prompt,
+    size='1024x1024',
+    n=1
+    
+)
+
+image_url = response["data"][0]["url"]
+pprint.pprint(response)
