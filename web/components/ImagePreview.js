07ca146318dc3b76784ca7a6f227801e6a609805
Jay Runkel <jayrunkel@gmail.com>
2024-01-20 03:44:37 -0500

    started to add props validation

--- web/components/ImagePreview.js
+++ web/components/ImagePreview.js
@@ -2,29 +2,35 @@
 // components/ImagePreview.tsx
 import React from "react";
 import Image from "next/image";
+import PropTypes from "prop-types";
 
 /*
-type Props = {
+type Props =
   images: File[];
 };
 */
 
 
 const ImagePreview = ({ images }) => {
-  return (
-    <div className="h-500">
-      <div className="grid grid-cols-12 gap-2 my-2">
-        {images.map((image) => {
-          const src = URL.createObjectURL(image);
-          return (
-            <div className="relative aspect-video col-span-4" key={image.name}>
-              <Image src={src} alt={image.name} className="object-cover" height="120" width="100" />
+    return (
+        <div className="h-500">
+            <div className="grid grid-cols-12 gap-2 my-2">
+                {images.map((image) => {
+                    const src = URL.createObjectURL(image);
+                    return (
+                        <div className="relative aspect-video col-span-4" key={image.name}>
+                            <Image src={src} alt={image.name} className="object-cover" height="120" width="100" />
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
+
+ImagePreview.propTypes = {
+    images: PropTypes.arrayOf(PropTypes.object)
+  }
+  
 
 export default ImagePreview;
--- web/components/Products.js
+++ web/components/Products.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Product from "./Product";
+import PropTypes from 'prop-types';
+
+
+
+  
 
 const Products = ({ products }) => {
+
   return (
     <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
       {products.map((product) => (
@@ -10,5 +16,11 @@ const Products = ({ products }) => {
     </div>
   );
 };
+
+
+Products.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.object)
+}
+
 
 export default Products;
